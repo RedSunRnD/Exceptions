cb98d358799bab87aead0cde4c290a673357ff01
RedSunRnD <vvatutin.rs@gmail.com>
2024-11-15 17:26:47 +0300

    Homework #1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserValidation[] Users = new UserValidation[]{
+                new UserValidation("TheBestLogin", "HardPassword_01", "HardPassword_01"),
+                new UserValidation("1111111", "22222222", "22222222")
+        };
+        try {
+            Users[1].validateUser();
+        } catch (Exception e) {
+            System.out.println("Ошибка валидации: " + e.getMessage());
+        } finally {
+            System.out.println("Процесс валидации завершен.");
+        }
     }
 }
